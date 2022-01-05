2937c7f9d02c67ca3e0406fa718e8530e9bbdfdd
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-01-04 20:21:38 -0500

    aosp -> lineage

--- lineage_begonia.mk
+++ lineage_begonia.mk
@@ -22,7 +22,7 @@ $(call inherit-product, device/xiaomi/begonia/device.mk)
 
 # Inherit some PE source stuff
 TARGET_BOOT_ANIMATION_RES := 1080
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Boot Animation
 TARGET_SCREEN_HEIGHT := 2340
@@ -30,7 +30,7 @@ TARGET_SCREEN_WIDTH := 1080
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := begonia
-PRODUCT_NAME := aosp_begonia
+PRODUCT_NAME := lineage_begonia
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 8 Pro
 PRODUCT_MANUFACTURER := Xiaomi
